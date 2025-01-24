eea14e7fde10ea20014409e32f94e5315f1bc800
myceey69 <126042972+myceey69@users.noreply.github.com>
2025-01-24 15:47:04 -0800

    Update Clock.cpp

--- Clock.cpp
+++ Clock.cpp
@@ -1,45 +1,50 @@
 #include <iostream> 
 #include <windows.h>
 
-using namespace std; 
+using namespace std;
 
-int main(){
+int main() {
 
-int h, m, s, a, err; 
-err=a=0; 
-while(err=0)
-{
-    cout << "enter hour: " << endl; 
-    cin >> h; 
-    cout << "enter minutes: " <<endl; 
-    cin >> m;
-    cout << "enter seconds: " <<endl; 
-    cin >> s;
-    if(h<24 && m < 60 && s < 60){ 
-        err++;
-    } else {
-        system("cls"); 
-    }
-    while(a==0){
-        system("cls"); 
-        cout <<h<<":" << m << ":" << s << endl; 
-        Sleep(1000);
-        s++; 
-        if(s>59){
-            s=0; 
-            m++;
+    int hrs, mins, sec, a, err;              //initialize variables
+    err = a = 0;
+    while (err == 0)
+    {
+        cout << "enter hour: " << endl;          //input hours
+        cin >> hrs;
+        cout << "enter minutes: " << endl;      //input minutes
+        cin >> mins;
+        cout << "enter seconds: " << endl;      //input seconds
+        cin >> sec;
+        if (hrs < 23 && mins < 60 && sec < 60) {
+            err++;
         }
-        if(m>59){
-            m=0;
-            h++; 
+        else {
+            system("cls");
         }
-        if(h>24){
-            h=0;
+        while (a == 0) {
+            system("cls");     // clears the screen everytime it loops. 
            
-        }
-    }
+            cout << "<==========>" << endl;
+            cout << "  " << hrs << ":" << mins << ":" << sec << endl;             //display and runs the clock with the numbers you inputted. 
+            cout << "<==========>" << endl; 
+           
+            Sleep(1000);            //changes the time by 1000 milliseconds which is 1 second. 
+            sec++;
+            if (sec > 59) {
+                sec = 0;
+                mins++;
+            }
+            if (mins > 59) {
+                mins = 0;
+                hrs++;
+            }
+            if (hrs > 23) {
+                hrs = 0;
 
-}
-return 0; 
-    
+            }
+        }
+
+    }
+    return 0;
+
 }
